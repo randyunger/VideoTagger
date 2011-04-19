e3b26da06655b5b33fe355fbc6db5c17b8a289bc
Randy Unger <randy.unger@gmail.com>
2011-04-18 23:25:16 -0700

    Moved to maven based arch

---
+++ src/main/java/test.scala
@@ -0,0 +1,11 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Randy
+ * Date: 4/18/11
+ * Time: 11:20 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+object test{
+  val x = 5
+}
